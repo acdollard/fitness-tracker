2d4379cac3d27b76180ed0d4c812bd52a08a9157
Alex Dollard <alexdollard141@gmail.com>
2020-03-10 18:11:13 -0500

    added some important code to server.js

--- server.js
+++ server.js
@@ -10,13 +10,18 @@ app.use(express.json());
 
 app.use(express.static("./public"));
 
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI)
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 
-require("./routes/routes")(app);
+require("./App/routes/routes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
